11c37d41d5d67ecc0f81a17516625bdd9cd4801b
cheon-eunsong <freesky111@naver.com>
2019-06-03 18:59:06 +0900

    camera2 & coroutines

--- app/src/main/java/com/es/camera_2/utils/ActivityExtensions.kt
+++ app/src/main/java/com/es/camera_2/utils/ActivityExtensions.kt
@@ -20,9 +20,6 @@ import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
 import android.widget.Toast
 
-/**
- * This file illustrates Kotlin's Extension Functions by extending FragmentActivity.
- */
 
 /**
  * Shows a [Toast] on the UI thread.
