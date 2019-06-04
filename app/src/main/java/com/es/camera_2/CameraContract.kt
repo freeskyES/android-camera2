02d33b9be469e2a3c10298fd130e3d6cbc1bc134
cheon-eunsong <freesky111@naver.com>
2019-06-04 14:50:07 +0900

    코드 정리

--- app/src/main/java/com/es/camera_2/CameraContract.kt
+++ app/src/main/java/com/es/camera_2/CameraContract.kt
@@ -1,8 +1,35 @@
 package com.es.camera_2
 
+import android.content.res.Resources
+import android.graphics.Matrix
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraManager
+import android.view.Display
+import java.io.File
+
 interface CameraContract {
 
     interface View{
+
+        fun getCameraManager(): CameraManager?
+
+        fun getDisplay(): Display?
+
+        fun setAspectRatioTextureView(width: Int, height: Int)
+
+        fun getResources(): Resources?
+
+        fun createFile(dirName: String, fileName: String): File
+
+        fun showErrorDialog()
+
+        fun setTransformTextureView(matrix: Matrix)
+
+        fun finish()
+
+        fun showToast(msg: String)
+
+        fun getSurfaceTexture(): SurfaceTexture
 
     }
 
@@ -15,6 +42,10 @@ interface CameraContract {
         fun onClickSwitchButton()
 
         fun onClickFlashButton()
+
+        fun onResume(textureView: AutoFitTextureView)
+
+        fun onDestroy()
 
     }
 }
